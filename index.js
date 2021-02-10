6b48ecf021aab4e45114f07b686414dea060c101
Valerie Ricot <valeriericot@gmail.com>
2021-02-10 12:29:06 -0500

    Added express middleware

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.get('/drinks', (req, res) => {
     res.send("GET /drinks response")
 })
 
 app.post('/drinks', (req, res) => {
-    res.send("POST /drinks response")
+    const { flavor, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${flavor} drinks!`)
 })
 
 app.listen(3000, () => {
