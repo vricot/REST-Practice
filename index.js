fe32fd935d38c808c2147f0daf73afd161cc029d
Valerie Ricot <valeriericot@gmail.com>
2021-02-10 17:50:57 -0500

    Added delete request using method-override

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Val',
@@ -58,6 +58,12 @@ app.get('/comments/:id/edit', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id)
     res.render('comments/edit', { comment })
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
 })
 
 app.patch('/comments/:id', (req, res) => {
