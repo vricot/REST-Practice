d2944e927ef71202054c1530aadc8383b43476e0
Valerie Ricot <valeriericot@gmail.com>
2021-02-10 17:27:52 -0500

    Installed and executed method-override for patch capabilities

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 const { v4: uuid } = require('uuid');
 uuid();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -50,6 +52,12 @@ app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id)
     res.render('comments/show', { comment })
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/edit', { comment })
 })
 
 app.patch('/comments/:id', (req, res) => {
