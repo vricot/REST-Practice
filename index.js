ba942e434f1fa7d1029764cea03597fe70fd0693
Valerie Ricot <valeriericot@gmail.com>
2021-02-10 15:48:58 -0500

    Added comment id's and referenced show.ejs for showing one full comment

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ app.set('views', path.join(__dirname, 'views'))
 
 const comments = [
     {
+        id: 1,
         username: 'Val',
         comment: 'lol so funny'
     },
     {
+        id: 2,
         username: 'Gregg',
         comment: 'I want pizza'
     },
     {
+        id: 3,
         username: 'xxSpacePrincessxx',
         comment: 'wut is so funny val?'
     },
     {
+        id: 4,
         username: 'onlymeows',
         comment: 'meowwwww purrr'
     }
@@ -38,6 +42,12 @@ app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
     comments.push({ username, comment })
     res.redirect("/comments");
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === parseInt(id))
+    res.render('comments/show', { comment })
 })
 
 app.get('/drinks', (req, res) => {
